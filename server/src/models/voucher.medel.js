2e9e67dd1c9123b2bc37aa16230762fd5d1002a5
Tan Phuc <phantanphuc282004@gmail.com>
2024-09-13 23:22:41 +0700

    voucher schema

---
+++ server/src/models/voucher.medel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const voucherSchema = new mongoose.Schema({
+    voucherName: {
+        type: String,
+        required: true
+    },
+    discount: {
+        type: Number, 
+        required: true
+    },
+    startDay: {
+        type: Date, 
+        required: true
+    },
+    endDay: {
+        type: Date,
+        required: true
+    },
+    code: {
+        type: String, 
+        maxlength: 5,
+        unique: true,
+        required: true
+    }
+}, { timestamps: true });
+
+const Voucher = mongoose.model('Voucher', voucherSchema);
+
+module.exports={Voucher}

6331b61af7cadbd646a1661fc58c6e77bd6b99b7
Tan Phuc <phantanphuc282004@gmail.com>
2024-09-25 00:28:54 +0700

    login owner and admin

--- client/src/pages/auth/LoginOwner.jsx
+++ client/src/pages/auth/LoginOwner.jsx
@@ -7,30 +7,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { openNotification } from "../../hooks/notification";
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { AuthContext } from "../../hooks/auth.context";
-function Login() {
+function LoginOwner() {
   const {auth,setAuth} = useContext(AuthContext)
   const [formLogin] = Form.useForm()
-  const email = Form.useWatch('email',formLogin)
+  const username = Form.useWatch('username',formLogin)
   const password = Form.useWatch('password',formLogin)
   const [errMessage,setErrMessage] = useState('')
   const navigate= useNavigate()
 
   axios.defaults.withCredentials=true
-  const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
-
   const handleLogin = (e) => {
     e.preventDefault()
-    if(!validateEmail(email)){
-      openNotification(false,"Email incorret","Enter your email")
-      return 
-    }
-    axios.post("http://localhost:4000/api/auth/signInCus",{email,password})
+    axios.post("http://localhost:4000/api/auth/signInOwner",{username,password})
       .then(res=>{
         console.log(res)
         if(res.data.login){
@@ -56,22 +44,22 @@ function Login() {
 
     <div className="h-screen w-full d-flex justify-content-center align-items-center bg-[url('https://wallpapercave.com/wp/wp9764093.jpg')] "  >
       <Form form={formLogin} className='py-[30px] h-[450px] w-[500px] border-2 border-[#777777 ] rounded-[30px] text-white bg-transparent backdrop-blur-[50px]'>
-        <h1 className=" mt-[10px] font-bold ">Login</h1>
+        <h1 className=" mt-[10px] font-bold ">Login Owner</h1>
 
         <Form.Item 
           className="m-[30px] mt-[40px] text-white"
-          label='Email'
-          name="email"
+          label='Username'
+          name="username"
           rules={[
             {
               required: true,
-              message: 'Please input your email!',
+              message: 'Please input your username!',
             },
           ]}
         >
           <Input
             className="bg-transparent max-w-[250px] rounded-[30px] px-[20px] py-[10px] text-white placeholder:text-white"
-            placeholder="Enter your email"
+            placeholder="Enter your password"
             suffix={<FontAwesomeIcon icon={faEnvelope} />}
           />
         </Form.Item>
@@ -88,7 +76,7 @@ function Login() {
         >
           <Input.Password
             className="bg-transparent max-w-[250px] rounded-[30px] px-[20px] py-[10px] text-white placeholder:text-white"
-            placeholder='Enter your password'
+            placeholder='Enter your username'
           />
         </Form.Item>
 
@@ -101,9 +89,8 @@ function Login() {
             onClick={handleLogin}
           >Login</button>
         </div>
-        <div className="my-[20px] mr-[30px] d-flex justify-between ">
+        <div className="my-[20px] mx-] d-flex justify-between ">
           <Link to='/register' className="text-white text-[15px] no-underline ml-[35px]">Don't you have account ?</Link>
-          <Link to='/loginOwner' className="text-white text-[15px] no-underline ml-[35px]">Login with owner or admin  ?</Link>
         </div>
 
       </Form>
@@ -112,4 +99,4 @@ function Login() {
   )
 }
 
-export default Login
+export default LoginOwner

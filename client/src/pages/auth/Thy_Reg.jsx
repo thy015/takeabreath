0c652965a28b1392c56a9fc2986f84a0ba0f4b62
ThyChan <thymai.1510@gmail.com>
2024-10-06 18:45:57 +0700

    change motion

--- client/src/pages/auth/Thy_Reg.jsx
+++ client/src/pages/auth/Thy_Reg.jsx
@@ -116,12 +116,16 @@ const Register = () => {
             </motion.div>
             <motion.div
               className="col-8"
-              initial={{ x: 0, opacity: 1 }}
+              initial={{opacity: 0 }}
               animate={{
-                x: isSignInClicked ? -600 : 0,
                 opacity: isSignInClicked ? 0 : 1,
               }}
-              transition={{ duration: 0.75, ease: easeIn }}
+              exit={{ opacity: 0 }}
+              transition={{
+                duration: 0.75,
+                ease: "easeOut",
+                delay: 0.15,
+              }}
               onAnimationComplete={() => {
                 if (isSignInClicked) {
                   navigate("/login");

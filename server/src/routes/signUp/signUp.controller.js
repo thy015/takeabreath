18698dbc60c98a853a816deb4cbf063232c6c973
Tan Phuc <phantanphuc282004@gmail.com>
2024-11-20 21:50:30 +0700

    fix logout 5

--- server/src/routes/signUp/signUp.controller.js
+++ server/src/routes/signUp/signUp.controller.js
@@ -352,7 +352,7 @@ const logout = async (req, res) => {
   console.log("[DOMAIN]",domain)
   console.log("[Token sso]", req.cookies.Token)
   try{
-    res.clearCookie('token',{ path: '/', domain: domain,secure: true}).json({ logout: true,message:"Delete token " + domain})
+    res.clearCookie('token',{ path: '/', domain: domain,secure: true, sameSite: 'None' }).json({ logout: true,message:"Delete token 5: " + domain})
   }catch(err){
     return res.json({ logout: err.message })
   }
